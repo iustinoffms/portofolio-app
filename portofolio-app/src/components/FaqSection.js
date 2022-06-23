cdbea086308ffb85a81b646e83ff6048aa203ff5
Iustin Sanduleanu <iustin.sanduleanu@thinslices.com>
2022-06-23 15:03:57 +0300

    implements html for main page

---
+++ portofolio-app/src/components/FaqSection.js
@@ -0,0 +1,53 @@
+import React from "react";
+
+const FaqSection = () => {
+  return (
+    <div className="faq">
+      <h2>
+        Any Questions <span>FAQ</span>
+      </h2>
+      <div className="question">
+        <h4>How do i start?</h4>
+        <div className="answer">
+          <p>The response right here</p>
+          <p>
+            The response right here and some more text because it's cool to have
+            more text
+          </p>
+        </div>
+      </div>
+      <div className="question">
+        <h4>Daily Schedule</h4>
+        <div className="answer">
+          <p>The response right here</p>
+          <p>
+            The response right here and some more text because it's cool to have
+            more text
+          </p>
+        </div>
+      </div>
+      <div className="question">
+        <h4>Different Payment Methonds</h4>
+        <div className="answer">
+          <p>The response right here</p>
+          <p>
+            The response right here and some more text because it's cool to have
+            more text
+          </p>
+        </div>
+      </div>
+      <div className="question">
+        <h4>What products do you offer?</h4>
+        <div className="answer">
+          <p>The response right here</p>
+          <p>
+            The response right here and some more text because it's cool to have
+            more text
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FaqSection;
